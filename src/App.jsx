ed819f5c51d56d4b298caa4104ea0155fbc4c020
Gabriel Coimbra <gabrielfelipe.coimbra@gmail.com>
2023-09-16 18:05:48 -0300

    Funcionalidade Filtrar Tarefas

--- src/App.jsx
+++ src/App.jsx
@@ -33,6 +33,8 @@ function App() {
 
   const [search, setSearch] = useState("");
 
+  const [filter, setFilter] = useState("All");
+  const [sort, setSort] = useState("Asc");
   // função cria um novo array com id aleatório
   const addTodo = (text, category) => {
     const newTodos = [
@@ -70,9 +72,16 @@ function App() {
     <div className="app">
       <h1>Lista de Tarefas</h1>
       <Search search={search} setSearch={setSearch} />
-      <Filter />
+      <Filter filter={filter} setFilter={setFilter} />
       <div className="todo-list">
         {todos
+          .filter((todo) =>
+            filter === "All"
+              ? true
+              : filter === "Completed"
+              ? todo.isCompleted
+              : !todo.isCompleted
+          )
           .filter((todo) =>
             todo.text.toLowerCase().includes(search.toLowerCase())
           )
--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,14 +1,14 @@
-const Filter = () => {
+const Filter = ({ filter, setFilter }) => {
   return (
     <div className="filter">
       <h2>Filtrar:</h2>
       <div className="filter-options">
         <div>
           <p>status:</p>
-          <select>
-            <option value="all">Todas</option>
-            <option value="completed">Completas</option>
-            <option value="incomplete">Incoompletas</option>
+          <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+            <option value="All">Todas</option>
+            <option value="Completed">Completas</option>
+            <option value="Incomplete">Incompletas</option>
           </select>
         </div>
         <div>
